ccee1702ab1404760aff2d72ddf1141cb81f8f71
Anurag kumar <112425461+anuragkoff@users.noreply.github.com>
2026-06-20 22:06:37 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start=0, end=s.length()-1;
+
+        while(end>start) {
+            while(!Character.isLetterOrDigit(s.charAt(start)) && start<end) {
+                start++;
+            }
+            while(!Character.isLetterOrDigit(s.charAt(end)) && start<end) {
+                end--;
+            }
+            if(start>end || (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end)))){
+                return false;
+            }
+            end--;
+            start++;
+        }
+        return true;
+    }
+}
